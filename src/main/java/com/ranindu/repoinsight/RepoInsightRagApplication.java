4205a58d5846d75ff91ce742d218e78a43a8c125
Ranindu Amarasinghe <raninduamarasinghe@gmail.com>
2026-04-03 08:08:00 +0530

    Initial commit: setup Spring Boot project with Web, WebFlux, Lombok, and DevTools

---
+++ src/main/java/com/ranindu/repoinsight/RepoInsightRagApplication.java
@@ -0,0 +1,13 @@
+package com.ranindu.repoinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoInsightRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoInsightRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranindu/repoinsight/RepoInsightRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranindu.repoinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoInsightRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
